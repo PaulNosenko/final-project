bf4b9173c0b78d1fe079c296b5436c9582019b82
Paul Nosenko <pavelgadzenko@gmail.com>
2023-07-25 09:15:56 +0200

    [master] - Added config

--- src/redux/tablesReducers.js
+++ src/redux/tablesReducers.js
@@ -1,3 +1,5 @@
+import { API_URL } from "../config";
+
 //actions
 const createActionName = actionName => `app/tables/${actionName}`;
 const UPDATE_TABLE = createActionName('UPDATE_TABLE');
@@ -12,7 +14,7 @@ export const updateTables = (payload) => ({ type: UPDATE_TABLES, payload })
 export const updateTable = (payload) => ({ type: UPDATE_TABLE, payload })
 export const fetchTables = () => {
     return (dispatch) => {
-        fetch('http://localhost:3131/api/tables')
+        fetch(`${API_URL}/tables`)
             .then(res => res.json())
             .then(tables => dispatch(updateTables(tables)));
     }
@@ -26,7 +28,7 @@ export const updateTableRequest = (payload, sideEffectFn) => {
             },
             body: JSON.stringify(payload)
         }
-        fetch(`http://localhost:3131/api/tables/${payload.id}`, options)
+        fetch(`${API_URL}/tables/${payload.id}`, options)
             .then(() => dispatch(updateTable(payload)))
             .then(() => sideEffectFn())
     };
